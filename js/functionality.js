456be820feb3529b2c486a6f04c21cdf5acaa1c7
alex-B-Ly <abl12582@gmail.com>
2015-11-16 04:12:06 -0500

    finished the lab, going to clear last row input fields on click, use regex for email validation and disable add button if not valid

--- js/functionality.js
+++ js/functionality.js
@@ -16,17 +16,18 @@ for(var i=0; i < deleteLength; i++){
 var adderButton = document.getElementById('adder');
 
 function addRow(){
-  var table = document.getElementById('customer-table');
+  var customerTable = document.getElementById('customer-table');
   var firstName = document.getElementById('first-name-entry');
   var lastName = document.getElementById('last-name-entry');
   var email = document.getElementById('email-entry');
 
 
-  var newRow = table.insertRow();
+  var newRow = customerTable.insertRow(customerTable.rows.length - 1);
   newRow.className += 'customer';
 
   for(var i=0; i<4; i++){
     var td = document.createElement('td');
+    var delButton = document.createElement('button');
 
     if(i===0){
       td.textContent = firstName.value;
@@ -34,6 +35,11 @@ function addRow(){
       td.textContent = lastName.value;
     }if(i===2){
       td.textContent = email.value;
+    }if(i===3){
+      delButton.className += 'btn btn-default remover';
+      delButton.textContent = 'X';
+      td.appendChild(delButton);
+      delButton.addEventListener('click', remover);
     }
 
     newRow.appendChild(td);
