e666f3817769585d9d5f7bf7055a2ccef0f79be3
alex-B-Ly <abl12582@gmail.com>
2015-11-17 05:46:06 -0500

    added random row function

--- js/functionality.js
+++ js/functionality.js
@@ -57,3 +57,30 @@ adderButton.addEventListener('click', addRow);
 
 // Random Second Add function
 
+(function(){
+  var customerTable = document.getElementById('customer-table');
+
+  setInterval(function(){
+  var randomRow = customerTable.insertRow(1);
+  randomRow.className += 'customer';
+
+    for(var i=0; i<4; i++){
+    var td = document.createElement('td');
+    var delButton = document.createElement('button');
+
+      if(i===0){
+        td.textContent = 'I am';
+      }if(i===1){
+        td.textContent = 'a randow';
+      }if(i===2){
+        td.textContent = 'row';
+      }if(i===3){
+        delButton.className += 'btn btn-default remover';
+        delButton.textContent = 'X';
+        td.appendChild(delButton);
+        delButton.addEventListener('click', remover);
+      }
+      randomRow.appendChild(td);
+    }
+  }, (Math.round(Math.random()*(10-5)+5))*1000);
+})();
